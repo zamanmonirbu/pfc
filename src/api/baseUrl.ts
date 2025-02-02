c6c52e476bb2262946ec0306412fd82be12b2d4e
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2025-02-02 17:21:04 +0600

    update base

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
 
-// export const BASE_URL = 'https://client-p9jo.onrender.com';
+export const BASE_URL = 'https://pfb-p6hu.onrender.com';
